a4da60c626bb7301e9a581be58f836ffd936652c
Ian Santos <iansantos@hotmail.com.br>
2017-08-17 23:08:21 -0300

    Melhorias e adição de funcionalidades: (#1)
    -Adicionado suporte a Banco de Dados para salvar e listar
    -A conexão com o banco de dados não requer o arquivo App.config
    -Realizadas melhorias na organização do código em todo o projeto

--- Cadastro de Clientes/CadastroClientes.cs
+++ Cadastro de Clientes/CadastroClientes.cs
@@ -1,7 +1,4 @@
-﻿// GEC5 AV1 Laboratório de Programação (.NET I) - Ian Santos / Data da última edição: 27/07/2016
-
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -9,7 +6,7 @@ namespace Cadastro_de_Clientes
 {
     public partial class CadastroClientes : Form
     {
-        private readonly List<Cliente> _listaClientes = new List<Cliente>();
+        private readonly string connstr = "Data Source = (localdb)\\mssqllocaldb; Initial Catalog = Cadastro_de_Clientes.Database; Integrated Security = True; MultipleActiveResultSets = True";
 
         public CadastroClientes()
         {
@@ -23,57 +20,28 @@ namespace Cadastro_de_Clientes
         private void salvarButton_Click(object sender, EventArgs e)
         {
             if (ValidaCadastro())
-            {
-                var random = new Random();  
-                var cliente = new Cliente
+                using (var bd = new Database(connstr))
                 {
-                    ClienteId = random.Next(1, int.MaxValue),
-                    Nome = nomeTextBox.Text.Trim(),
-                    Cpf = cpfmaskedTextBox.Text,
-                    Endereco = enderecoTextBox.Text.Trim(),
-                    Telefone = telefoneMaskedTextBox.Text
-                };
-                DialogResult = MessageBox.Show(@"Deseja adicionar?" + Environment.NewLine + Environment.NewLine + @"Nome: " + cliente.Nome + Environment.NewLine + @"CPF: " + cliente.Cpf + Environment.NewLine + @"Endereço: " + cliente.Endereco + Environment.NewLine + @"Telefone: " + cliente.Telefone, @"Confirmação de Cadastro", MessageBoxButtons.YesNo);
-                if (DialogResult == DialogResult.Yes)
-                {
-                    _listaClientes.Add(cliente);
-                    MessageBox.Show(@"Cliente adicionado.", @"Confirmação de Cadastro", MessageBoxButtons.OK);
-                    foreach (var textBox in Controls.OfType<TextBox>())
+                    var cliente = new Cliente
                     {
-                        textBox.Clear();
+                        Nome = nomeTextBox.Text.Trim(),
+                        Cpf = cpfmaskedTextBox.Text,
+                        Endereco = enderecoTextBox.Text.Trim(),
+                        Telefone = telefoneMaskedTextBox.Text
+                    };
+                    DialogResult = MessageBox.Show(@"Deseja adicionar?" + Environment.NewLine + Environment.NewLine + @"Nome: " + cliente.Nome + Environment.NewLine + @"CPF: " + cliente.Cpf + Environment.NewLine + @"Endereço: " + cliente.Endereco + Environment.NewLine + @"Telefone: " + cliente.Telefone, @"Confirmação de Cadastro", MessageBoxButtons.YesNo);
+                    if (DialogResult == DialogResult.Yes)
+                    {
+                        bd.Clientes.Add(cliente);
+                        bd.SaveChanges();
+                        MessageBox.Show(@"Cliente adicionado.", @"Confirmação de Cadastro", MessageBoxButtons.OK);
+                        ApagarCampos();
                     }
-                    foreach (var maskedTextBox in Controls.OfType<MaskedTextBox>())
+                    else
                     {
-                        maskedTextBox.Clear();
+                        MessageBox.Show(@"Cliente não adicionado.", @"Confirmação de Cadastro", MessageBoxButtons.OK);
                     }
                 }
-                else
-                {
-                    MessageBox.Show(@"Cliente não adicionado.", @"Confirmação de Cadastro", MessageBoxButtons.OK);
-                }
-            }
-        }
-
-        #endregion
-
-        #region Código responsável por apagar todos os campos
-
-        private void limparButton_Click(object sender, EventArgs e)
-        {
-            foreach (var textBox in cadastroGroupBox.Controls.OfType<TextBox>())
-            {
-                textBox.Clear();
-            }
-
-            foreach (var maskedTextBox in cadastroGroupBox.Controls.OfType<MaskedTextBox>())
-            {
-                maskedTextBox.Clear();
-            }
-
-            foreach (var label in infoGroupBox.Controls.OfType<Label>())
-            {
-                label.Visible = false;
-            }
         }
 
         #endregion
@@ -82,17 +50,31 @@ namespace Cadastro_de_Clientes
 
         private void listarButton_Click(object sender, EventArgs e)
         {
-            if (_listaClientes.Count > 0)
-            {
-                foreach (var cliente in _listaClientes.OrderBy(cliente => cliente.ClienteId))
-                {
+            var bd = new Database(connstr);
+            if (bd.Clientes.Any())
+                foreach (var cliente in bd.Clientes.OrderBy(cliente => cliente.ClienteId))
                     MessageBox.Show(@"Nome: " + cliente.Nome + Environment.NewLine + @"CPF: " + cliente.Cpf + Environment.NewLine + @"Endereço: " + cliente.Endereco + Environment.NewLine + @"Telefone: " + cliente.Telefone, @"Cliente ID " + cliente.ClienteId, MessageBoxButtons.OK);
-                }
-            }
             else
-            {
-                MessageBox.Show(@"Não há clientes cadastrados.", @"Lista Vazia", MessageBoxButtons.OK, MessageBoxIcon.Information);
-            }
+                MessageBox.Show(@"Não há clientes cadastrados.", @"Database Vazia", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        #endregion
+
+        #region Código responsável por apagar todos os campos
+
+        private void limparButton_Click(object sender, EventArgs e)
+        {
+            ApagarCampos();
+        }
+
+        private void ApagarCampos()
+        {
+            foreach (var textBox in cadastroGroupBox.Controls.OfType<TextBox>())
+                textBox.Clear();
+            foreach (var maskedTextBox in cadastroGroupBox.Controls.OfType<MaskedTextBox>())
+                maskedTextBox.Clear();
+            foreach (var label in infoGroupBox.Controls.OfType<Label>())
+                label.Visible = false;
         }
 
         #endregion
@@ -101,26 +83,22 @@ namespace Cadastro_de_Clientes
 
         private void nomeTextBox_KeyPress(object sender, KeyPressEventArgs e)
         {
-            e.Handled = !(char.IsLetter(e.KeyChar) || (e.KeyChar == (char) Keys.Space) || (e.KeyChar == (char) Keys.Back));
+            e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char) Keys.Space || e.KeyChar == (char) Keys.Back);
         }
 
         private void enderecoTextBox_KeyPress(object sender, KeyPressEventArgs e)
         {
-            e.Handled = !(char.IsLetter(e.KeyChar) || (char.IsNumber(e.KeyChar) || (e.KeyChar == (char) Keys.Space)) || (e.KeyChar == (char) Keys.Back) || (e.KeyChar == ','));
+            e.Handled = !(char.IsLetter(e.KeyChar) || char.IsNumber(e.KeyChar) || e.KeyChar == (char) Keys.Space || e.KeyChar == (char) Keys.Back || e.KeyChar == ',');
         }
 
-        private Boolean ValidaCadastro()
+        private bool ValidaCadastro()
         {
             nomeErrorLabel.Visible = nomeTextBox.TextLength < 10 || nomeTextBox.Text.Distinct().Count() < 2;
-            cpfErrorLabel.Visible = cpfmaskedTextBox.Text.Replace(" ", "").Length < 11 || cpfmaskedTextBox.Text.Replace(" ","").Distinct().Count() < 2;
+            cpfErrorLabel.Visible = cpfmaskedTextBox.Text.Replace(" ", "").Length < 11 || cpfmaskedTextBox.Text.Replace(" ", "").Distinct().Count() < 2;
             enderecoErrorLabel.Visible = enderecoTextBox.Text.Length < 10 || enderecoTextBox.Text.Distinct().Count() < 2;
             telefoneErrorLabel.Visible = telefoneMaskedTextBox.Text.Replace(" ", "").Length < 10 || telefoneMaskedTextBox.Text.Replace(" ", "").Distinct().Count() < 2;
-
             if (nomeErrorLabel.Visible || cpfErrorLabel.Visible || enderecoErrorLabel.Visible || telefoneErrorLabel.Visible)
-            {
                 return false;
-            }
-
             return true;
         }
 
---
+++ Cadastro de Clientes/Database.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace Cadastro_de_Clientes
+{
+    public class Database : DbContext
+    {
+        public Database(string connectionString) : base(connectionString)
+        {
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
